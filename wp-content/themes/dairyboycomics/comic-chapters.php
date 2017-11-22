1e4cdc82a6fb519a66e439fe598ff2ba767b319e
Austin Verburg <dairyboycomics@gmail.com>
2017-11-21 22:40:41 -0800

    starts of canto and story arcs added in info

--- wp-content/themes/dairyboycomics/comic-chapters.php
+++ wp-content/themes/dairyboycomics/comic-chapters.php
@@ -19,6 +19,7 @@ get_header();
 
 		        the_post_thumbnail('thumbnail'); 
 				
+				//Get Canto info
 				$terms = get_the_terms(get_the_ID(), 'chapters');
 				$i = 0;
 				foreach($terms as $term) {
@@ -34,34 +35,66 @@ get_header();
 
 				<?php
 				//Date of first comic in canto
-				$args2 = array (
-					'post_type'		=> 'comic',
-					'order'			=> 'ASC',
-					'chapters' 		=> $cantoSlug,
+				$firstDateArg = array (
+					'post_type'			=> 'comic',
+					'order'				=> 'ASC',
+					'chapters' 			=> $cantoSlug,
                     'posts_per_page'    => 1
                 );
-                $firstDate = new WP_Query( $args2 );
+                $firstDate = new WP_Query( $firstDateArg );
                 if ( $firstDate -> have_posts() ) :
                     while ( $firstDate -> have_posts() ) : $firstDate -> the_post();
                         $startDate = get_the_date('M j, Y');
+                        $firstPage = get_permalink();
                     endwhile;
                 endif;
 
                 //Date of last comic in canto
-				$args3 = array (
-					'post_type'		=> 'comic',
-					'order'			=> 'DESC',
-					'chapters' 		=> $cantoSlug,
+				$lastDateArg = array (
+					'post_type'			=> 'comic',
+					'chapters' 			=> $cantoSlug,
                     'posts_per_page'    => 1
                 );
-                $secondDate = new WP_Query( $args3 );
-                if ( $secondDate -> have_posts() ) :
-                    while ( $secondDate -> have_posts() ) : $secondDate -> the_post();
+                $lastDate = new WP_Query( $lastDateArg );
+                if ( $lastDate -> have_posts() ) :
+                    while ( $lastDate -> have_posts() ) : $lastDate -> the_post();
                         $endDate = get_the_date('M j, Y');
                     endwhile;
                 endif; ?>
 
                 <p><?php echo $startDate . ' - ' . $endDate; ?></p>
+
+				<ul>
+					<li>
+						<a href="<?php echo $firstPage; ?>">First page of canto</a>
+					</li>
+					<?php
+					//Get start of story arcs within canto
+					$storyArgs = array (
+						'post_type'		=> 'comic',
+						'order'			=> 'ASC',
+						'chapters' 		=> $cantoSlug,
+	                   	'meta_query' => array(
+					        array(
+					            'key'     => 'story_start',
+					            'value'   => 'yes',
+					            'compare' => 'LIKE'
+					        )
+					    )
+	                );
+	                $storyArcs = new WP_Query( $storyArgs );
+	                if ( $storyArcs -> have_posts() ) : ?>
+						<li>Story arc starting pages:</li>
+							<ul>
+	                    <?php while ( $storyArcs -> have_posts() ) : $storyArcs -> the_post(); ?>
+	                        <li>
+	                        	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	                        </li>
+	                    <?php
+	                    endwhile; ?>
+	                    	</ul>
+	                <?php endif; ?>
+                </ul>
 
 	    <?php endwhile;
 	else: 
