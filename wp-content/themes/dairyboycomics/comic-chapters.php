2a1fa64fb5d36c19328903f14905c06b301dba5a
Austin Verburg <dairyboycomics@gmail.com>
2017-11-21 21:11:18 -0800

    display pages within each canto

--- wp-content/themes/dairyboycomics/comic-chapters.php
+++ wp-content/themes/dairyboycomics/comic-chapters.php
@@ -6,37 +6,41 @@ get_header();
 
 	$args = array(
 				'post_type'		=> 'comic',
+				'order'			=> 'ASC',
 				'chapters' 		=> 'austins-inferno',
 				'meta_key' 		=> 'is_cover',
 				'meta_value' 	=> 'yes'
 			);
-	   $category_posts = new WP_Query($args);
+	$category_posts = new WP_Query($args);
 
-	   if($category_posts->have_posts()) : 
-	      while($category_posts->have_posts()) : 
-	         $category_posts->the_post(); 
+	if($category_posts->have_posts()) : 
+	    while($category_posts->have_posts()) : 
+	        $category_posts->the_post(); 
 
-	         the_post_thumbnail('thumbnail'); 
-			
-			$terms = get_the_terms(get_the_ID(), 'chapters');
-			$i = 0;
-			foreach($terms as $term) {
-				if ($i == 0) {
-					echo "";
-				} else {
-					echo $term->name;
+		        the_post_thumbnail('thumbnail'); 
+				
+				$terms = get_the_terms(get_the_ID(), 'chapters');
+				$i = 0;
+				foreach($terms as $term) {
+					if ($i == 0) {
+						echo "";
+					} else {
+						echo '<p>' . $term->name . '</p>';
+						echo '<p>Pages: ' . $term->count . '</p>';
+					}
+					$i++;
 				}
-				$i++;
-			}
 
-	      endwhile;
-	   else: 
+
+
+	    endwhile;
+	else: 
 	?>
 
-	      Oops, there are no posts.
+	    Oops, there are no posts.
 
 	<?php
-	   endif;
+	endif;
 
 	wp_reset_postdata();
 	wp_reset_query();
