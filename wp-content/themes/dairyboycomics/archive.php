6e303698eade0b03a6fe85f9a9a5b2096e88b73b
Austin Verburg <dairyboycomics@gmail.com>
2017-12-01 13:12:02 -0800

    archive pulls comics with pagination

--- wp-content/themes/dairyboycomics/archive.php
+++ wp-content/themes/dairyboycomics/archive.php
@@ -1,6 +1,75 @@
 <?php
 get_header();
 
-echo "yolo";
+	$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+	$series = $_GET['series'];
+	$character = $_GET['character'];
+	$tag = $_GET['tag'];
+	$date = $_GET['date'];
+
+	if ($comic == 'all') {
+		$comic = '';
+	}
+	
+	$args = array(
+				'post_type'		=> 'comic',
+				'order'			=> 'ASC',
+				'posts_per_page'=> 12,
+				'paged'			=> $paged
+			);
+	$category_posts = new WP_Query($args);
+
+	if($category_posts->have_posts()) : 
+
+		next_posts_link();
+		previous_posts_link();
+
+		$x = 0 ?>
+	
+		<div class="row">
+
+	    <?php while($category_posts->have_posts()) : 
+	        $category_posts->the_post();
+
+				if ($x == 4) { ?>
+					</div>
+					<div class="row">
+				<?php }
+
+				$x++; ?>
+
+				<div class="col-sm-3">
+
+					<a href="<?php echo the_permalink(); ?>">
+						<img src="<?php echo the_post_thumbnail_url( 'thumbnail' ); ?>" alt="<?php the_title(); ?>">						
+					</a>
+
+					<p>
+						<a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a>		
+					</p>
+
+					<p>
+						<?php echo get_the_date(); ?>
+					</p>
+
+	            </div>
+
+		<?php endwhile; ?>
+
+		<?php next_posts_link(); ?>
+		<?php previous_posts_link(); ?>
+
+	    </div>
+
+	<?php else: ?>
+
+	    Oops, there are no posts.
+
+	<?php
+	endif;
+
+	wp_reset_postdata();
+	wp_reset_query();
 
 get_footer(); ?>
