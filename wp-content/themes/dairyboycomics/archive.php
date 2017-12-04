79cfc72d3e9faaca48275d029c490a079e4c4b42
Austin Verburg <dairyboycomics@gmail.com>
2017-12-04 15:24:54 -0800

    started search form, search by series and character

--- wp-content/themes/dairyboycomics/archive.php
+++ wp-content/themes/dairyboycomics/archive.php
@@ -1,6 +1,74 @@
 <?php
-get_header();
+get_header(); ?>
 
+	<form class="" action="" method="get">
+
+		<div class="option-bar">
+			<label for="select-comic">Comic</label>
+	    	<span class="selectwrap">
+	        	<select name="title" id="select-comic" class="search-select" tabindex="-1" aria-hidden="true">
+		        	<option value="all" selected="selected">All</option>
+
+					<?php
+					$taxonomy = 'chapters';
+					$term_args = array(
+						'parent' => 0,
+						'order' => 'DESC'
+					);
+					$tax_terms = get_terms($taxonomy, $term_args);
+					?>
+					<ul>
+					<?php
+					foreach ($tax_terms as $tax_term) {  ?>
+					        <option value="<?php echo $tax_term->slug; ?>"><?php echo $tax_term->name; ?></option>
+					<?php } ?>
+
+		        </select>
+	    	</span>
+		</div>
+
+		<div class="option-bar large">
+			<label for="select-character">Character</label>
+	    	<span class="selectwrap">
+	        	<select name="characterId" id="characterId" class="search-select select2-hidden-accessible" tabindex="-1" aria-hidden="true">
+		        	
+					<option value="all" selected="selected">All</option>
+
+					<?php 
+
+					$args = array(
+		                'post_type' => 'characters',
+		                'posts_per_page' => -1	                	
+	                );
+
+			        $posts = new WP_Query($args);
+			 
+			        if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post(); ?>
+
+			        	<option value="<?php the_ID(); ?>"> <?php the_title(); ?> </option>  
+
+				    <?php
+
+				    endwhile; 
+
+				    wp_reset_postdata();
+				    wp_reset_query();
+
+				    endif; 
+
+				    ?>
+
+		        </select>
+	    	</span>
+		</div> 
+
+		<div class="option-bar">
+				<input type="submit" value="Search" class="real-btn btn">
+		</div>	
+
+	</form>
+
+	<?php
 	$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 
 	$series = $_GET['series'];
@@ -15,7 +83,7 @@ get_header();
 	$args = array(
 				'post_type'		=> 'comic',
 				'order'			=> 'ASC',
-				'posts_per_page'=> 12,
+				'posts_per_page'=> 10,
 				'paged'			=> $paged
 			);
 	$category_posts = new WP_Query($args);
