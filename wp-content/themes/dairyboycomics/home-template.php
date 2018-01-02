f8e7cedf010d1a1d1e0e0d4283d114c544f68b45
Austin Verburg <dairyboycomics@gmail.com>
2018-01-02 13:11:21 -0800

    comic and sidebar added to home

--- wp-content/themes/dairyboycomics/home-template.php
+++ wp-content/themes/dairyboycomics/home-template.php
@@ -4,6 +4,26 @@ Template Name: Home
 */
 get_header(); ?>
 
-<h1>yolo</h1>
+<div id="comic">
+	<?php
+		$args = array(
+			'post_type'		=> 'comic',
+			'posts_per_page'=> 1,
+			'chapters' 		=> 'austins-inferno',
+		);
+		$query_posts = new WP_Query($args);
+
+		if($query_posts->have_posts()) : while($query_posts->have_posts()) : $query_posts->the_post(); ?>
+			
+			<a href="<?php get_post_permalink(); ?>">
+				<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="comic">
+			</a>
+
+		<?php endwhile; endif; ?>
+</div>
+
+<div id="sidebar-right-of-comic" class="sidebar">
+	<img src="https://dairyboy-comics.dev/wp-content/uploads/2017/11/twitter-pic-300x300.jpg" width="202" height="202" alt="">
+</div>
 
 <?php get_footer();
