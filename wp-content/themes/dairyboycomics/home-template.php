b1d2d20133f216b01355694213e1ac8b2c419346
Austin Verburg <dairyboycomics@gmail.com>
2018-01-03 15:16:56 -0800

    hover text added to home comic

--- wp-content/themes/dairyboycomics/home-template.php
+++ wp-content/themes/dairyboycomics/home-template.php
@@ -16,17 +16,23 @@ get_header(); ?>
 		);
 		$query_posts = new WP_Query($args);
 
-		if($query_posts->have_posts()) : while($query_posts->have_posts()) : $query_posts->the_post(); ?>
+		if($query_posts->have_posts()) : while($query_posts->have_posts()) : $query_posts->the_post();
+
+			$altText = get_post_meta (get_the_ID(), 'comic-hovertext', true) ?>
 			
 			<a href="<?php get_post_permalink(); ?>">
-				<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+				<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo $altText; ?>" title="<?php echo $altText; ?>">
 			</a>
 
 		<?php endwhile; endif; ?>
 </div>
 
 <div id="sidebar-right-of-comic" class="sidebar">
-	<img src="https://dairyboy-comics.dev/wp-content/uploads/2017/11/twitter-pic-300x300.jpg" width="202" height="202" alt="">
+	<div class="home-sidebar">
+		<div class="social-links">
+			<div style=""></div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer();
