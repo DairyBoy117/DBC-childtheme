376dd09edabeb3b2f71ba500242d2e2fffaa8b2d
Austin Verburg <dairyboycomics@gmail.com>
2018-01-27 22:01:41 -0800

    heading font updated

--- wp-content/themes/dairyboycomics/functions.php
+++ wp-content/themes/dairyboycomics/functions.php
@@ -4,6 +4,7 @@ function theme_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', 
         array('parent-style') );
+    wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Paytone+One', false );
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles');
 
