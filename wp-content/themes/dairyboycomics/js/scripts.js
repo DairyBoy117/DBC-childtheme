2c4513bd923f52dcec7578eda0d545648650a312
Austin Verburg <dairyboycomics@gmail.com>
2018-02-01 20:05:17 -0800

    lightbox functions

--- wp-content/themes/dairyboycomics/js/scripts.js
+++ wp-content/themes/dairyboycomics/js/scripts.js
@@ -6,32 +6,15 @@ jQuery().ready(function($) {
 		
 			e.preventDefault();
 			
-			//Get clicked link href
 			var image_href = $(this).attr("href");
-			
-			/* 	
-			If the lightbox window HTML already exists in document, 
-			change the img src to to match the href of whatever link was clicked
-			
-			If the lightbox window HTML doesn't exists, create it and insert it.
-			(This will only happen the first time around)
-			*/
-			
-			if ($('#lightbox').length > 0) { // #lightbox exists
-				
-				//place href as img src value
-				$('#content').html('<img src="' + image_href + '" />');
-			   	
-				//show lightbox window - you could use .show('fast') for a transition
+
+			if ($('#lightbox').length > 0) {
 				$('#lightbox').show();
 			}
 			
-			else { //#lightbox does not exist - create and insert (runs 1st time only)
-				
-				//create HTML markup for lightbox window
+			else { 
 				var lightbox = 
 				'<div id="lightbox">' +
-					'<p>Click to close</p>' +
 					'<div id="content">' + //insert clicked link's href into img src
 						'<img src="' + image_href +'" />' +
 					'</div>' +	
@@ -39,6 +22,10 @@ jQuery().ready(function($) {
 					
 				//insert lightbox HTML into page
 				$('body').append(lightbox);
+
+				var winHeight = $( window ).height();
+
+				$('#lightbox img').css('max-height', winHeight);
 			}
 			
 		});
