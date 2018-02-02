5860ff617937c10ea0fa1350de5af93bf290b329
Austin Verburg <dairyboycomics@gmail.com>
2018-02-01 22:20:00 -0800

    unique thumbnails for character preview added

--- wp-content/themes/dairyboycomics/single-characters.php
+++ wp-content/themes/dairyboycomics/single-characters.php
@@ -4,113 +4,116 @@
 
 		while (have_posts()) : the_post(); ?>
 
-			<div class="row">
-				<div class="col-sm-6">
-					<h1><?php the_title(); ?></h1>
+			<div class="comic-series">
 
-					<?php if ( has_term( 'austins-inferno', 'from_comic' ) ) { 
+				<div class="row">
+					<div class="col-xs-6">
+						<h1><?php the_title(); ?></h1>
 
-						$grade = get_field('grade');
-						$weapon = get_field('weapon_of_choice');
-						$powers = get_field('super_powers');
-						$question = get_field('random_question');
-						$answer = get_field('random_answer');
+						<?php if ( has_term( 'austins-inferno', 'from_comic' ) ) { 
 
-						if ($grade) { ?>
-							<p>Grade: <?php echo $grade; ?></p>
-						<?php }
+							$grade = get_field('grade');
+							$weapon = get_field('weapon_of_choice');
+							$powers = get_field('super_powers');
+							$question = get_field('random_question');
+							$answer = get_field('random_answer');
 
-						if ($weapon) { ?>
-							<p>Weapon of choice: <?php echo $weapon; ?></p>
-						<?php }
+							if ($grade) { ?>
+								<p>Grade: <?php echo $grade; ?></p>
+							<?php }
 
-						if ($powers) { ?>
-							<p>Super Powers: <?php echo $powers; ?></p>
-						<?php }
+							if ($weapon) { ?>
+								<p>Weapon of choice: <?php echo $weapon; ?></p>
+							<?php }
 
-						if ($question) { ?>
-							<p><?php echo $question; ?>: <?php echo $answer; ?></p>
-						<?php } 
+							if ($powers) { ?>
+								<p>Super Powers: <?php echo $powers; ?></p>
+							<?php }
+
+							if ($question) { ?>
+								<p><?php echo $question; ?>: <?php echo $answer; ?></p>
+							<?php } 
+						
+						}
+
+						if ( has_term( 'halo-pwned', 'from_comic' ) ) {
+
+							$species = get_field('species');
+							$faction = get_field('faction');
+							$playedBy = get_field('played_by');
+							$player = $playedBy[0];
+							$halo = get_field('halo_bio');
+
+							if ($species) { ?>
+								<p>Species: <?php echo $species; ?></p>
+							<?php }
+
+							if ($faction) { ?>
+								<p>Faction: <?php echo $faction; ?></p>
+							<?php }
+
+							if ($playedBy) { ?>
+								<p>Played By: <a href="<?php echo get_permalink($player); ?>"><?php echo get_the_title($player); ?></a></p>
+							<?php }
+
+							if ($halo) { ?>
+								<a href="<?php echo $halo; ?>">Original Character's Bio</a>
+							<?php } 
+
+						} ?>
+
+						<a href="<?php echo get_site_url(); ?>/comic/?characterId=<?php echo get_the_ID(); ?>">View Comics</a>
 					
-					}
-
-					if ( has_term( 'halo-pwned', 'from_comic' ) ) {
-
-						$species = get_field('species');
-						$faction = get_field('faction');
-						$playedBy = get_field('played_by');
-						$player = $playedBy[0];
-						$halo = get_field('halo_bio');
-
-						if ($species) { ?>
-							<p>Species: <?php echo $species; ?></p>
-						<?php }
-
-						if ($faction) { ?>
-							<p>Faction: <?php echo $faction; ?></p>
-						<?php }
-
-						if ($playedBy) { ?>
-							<p>Played By: <a href="<?php echo get_permalink($player); ?>"><?php echo get_the_title($player); ?></a></p>
-						<?php }
-
-						if ($halo) { ?>
-							<a href="<?php echo $halo; ?>">Original Character's Bio</a>
-						<?php } 
-
-					} ?>
-
-					<a href="<?php echo get_site_url(); ?>/comic/?characterId=<?php echo get_the_ID(); ?>">View Comics</a>
-				
-				</div>
-				<div class="col-sm-6">
-					
-					<?php the_post_thumbnail( 'medium' ); ?>
-
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-sm-12">
-					<?php the_content(); ?>
-				</div>
-			</div>
-
-			<?php
-
-			if ( has_term( 'austins-inferno', 'from_comic' ) ) {
-				$comic = 'austins-inferno';
-			} else if ( has_term( 'halo-pwned', 'from_comic' ) ) {
-				$comic = 'halo-pwned';
-			}
-
-			//All other characters
-			$otherCharacters = array (
-				'post_type'			=> 'characters',
-				'orderby'			=> 'title',
-				'order'				=> 'ASC',
-				'from_comic'		=> $comic,
-                'posts_per_page'    => -1
-            );
-            $characters = new WP_Query( $otherCharacters );
-            if ( $characters -> have_posts() ) : ?>
-
-            	<div class="row">
-                
-                <?php while ( $characters -> have_posts() ) : $characters -> the_post(); ?>
-                    <div class="col-sm-2">
-                    	<a href="<?php the_permalink(); ?>">
-                    		<?php the_post_thumbnail( array(50,50) ); ?>
-							<?php the_title(); ?>
-                    	</a>
 					</div>
-                <?php endwhile; ?>
+					<div class="col-xs-6">
+						
+						<?php the_post_thumbnail( 'medium' ); ?>
 
-            	</div>
+					</div>
+				</div>
 
-            <?php endif;
+				<div class="row">
+					<div class="col-xs-12">
+						<?php the_content(); ?>
+					</div>
+				</div>
 
-		endwhile;
+				<?php
+
+				if ( has_term( 'austins-inferno', 'from_comic' ) ) {
+					$comic = 'austins-inferno';
+				} else if ( has_term( 'halo-pwned', 'from_comic' ) ) {
+					$comic = 'halo-pwned';
+				}
+
+				//All other characters
+				$otherCharacters = array (
+					'post_type'			=> 'characters',
+					'orderby'			=> 'title',
+					'order'				=> 'ASC',
+					'from_comic'		=> $comic,
+	                'posts_per_page'    => -1
+	            );
+	            $characters = new WP_Query( $otherCharacters );
+	            if ( $characters -> have_posts() ) : ?>
+
+	            	<div class="row">
+	                
+	                <?php while ( $characters -> have_posts() ) : $characters -> the_post(); ?>
+	                    <div class="col-xs-1">
+	                    	<a href="<?php the_permalink(); ?>">
+	                    		<img src="<?php echo get_field('character_thumbnail'); ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" height="50" width="50">
+	                    	</a>
+						</div>
+	                <?php endwhile; ?>
+
+	            	</div>
+
+	            <?php endif; ?>
+
+            </div>
+
+		<?php endwhile;
 	
 	}
 
