1f983f902844d48cf8ab3589f89af842004df531
Austin Verburg <dairyboycomics@gmail.com>
2018-02-10 15:42:38 -0800

    redirect if on mobile

--- wp-content/themes/dairyboycomics/ninth-circle.php
+++ wp-content/themes/dairyboycomics/ninth-circle.php
@@ -16,101 +16,120 @@ Template Name: Ninth Circle
 </head>
 <body <?php body_class(); ?>>
 
-	<?php 
+	<?php
+	if ( wp_is_mobile() ) { ?>
+	    <h1>Your device cannot fathom the information contained within this page. Redirecting you back to home...</h1>
+
+	    <a href="<?php echo site_url(); ?>">
+	    	<h1>If redirection doesn't work, click here</h1>
+	    </a>
+
+	    <script>
+	    	function forceLeave() {
+				window.location.replace("<?php echo site_url(); ?>");
+			}
+			setTimeout(function () {
+		        forceLeave();
+		    }, 3000);
+	    </script>
+	<?php } else {
+
 		$t = current_time('H');
 
 		if ($t == 15) {
 			$heIsHere = true;
 		} else {
 			$heIsHere = false;
-		}
-	?>
-	<a href="<?php echo site_url(); ?>">TURN BACK</a>
+		} ?>
 
-	<img id="ground" 
-	src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/ground.png">
+		<a href="<?php echo site_url(); ?>" id="turnBack">TURN BACK</a>
 
-	<img id="left-corner" 
-	src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/left-corner.png">
+		<img id="ground" 
+		src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/ground.png">
 
-	<img id="right-corner" 
-	src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/right-corner.png">
+		<img id="left-corner" 
+		src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/left-corner.png">
 
-	<?php if ($heIsHere) { ?>
+		<img id="right-corner" 
+		src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/right-corner.png">
 
-		<img id="prisoner" 
-		src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/prisoner.gif">
-		<script> document.getElementById('prisoner').ondragstart = function() { return false; }; </script>	
+		<?php if ($heIsHere) { ?>
 
-		<audio id="darkness" 
-		src="<?php echo get_stylesheet_directory_uri() ?>/media/Gathering_Darkness.mp3" 
-		autoplay loop></audio>
+			<img id="prisoner" 
+			src="<?php echo get_stylesheet_directory_uri() ?>/img/ninth-circle/prisoner.gif">
+			<script> document.getElementById('prisoner').ondragstart = function() { return false; }; </script>	
 
-	<?php } else { ?>
+			<audio id="darkness" 
+			src="<?php echo get_stylesheet_directory_uri() ?>/media/Gathering_Darkness.mp3" 
+			autoplay loop></audio>
 
-		<audio id="silence" 
-		src="<?php echo get_stylesheet_directory_uri() ?>/media/Silence.mp3" 
-		autoplay loop></audio>
+		<?php } else { ?>
+
+			<audio id="silence" 
+			src="<?php echo get_stylesheet_directory_uri() ?>/media/Silence.mp3" 
+			autoplay loop></audio>
+
+		<?php } ?>
+
+		<!-- You belong to your father, the devil, and you want to carry out your father’s desires. -John 8:44 -->
+
+		<script>
+
+			document.getElementById('ground').ondragstart = function() { return false; };
+			document.getElementById('left-corner').ondragstart = function() { return false; };
+			document.getElementById('right-corner').ondragstart = function() { return false; };
+
+		    var audio = document.getElementById("darkness");
+		    audio.volume = 0.2
+		    ;
+
+		    function imgResize() {
+		  	    var myHeight = 0;
+			    if( typeof( window.innerWidth ) == 'number' ) {
+			      //Non-IE
+			      myHeight = window.innerHeight;
+			    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+			      //IE 6+ in 'standards compliant mode'
+			      myHeight = document.documentElement.clientHeight;
+			    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+			      //IE 4 compatible
+			      myHeight = document.body.clientHeight;
+			    }
+			    var img = document.getElementById('ground');
+			    var imgheight = myHeight + 'px';
+			    img.style.height = imgheight;
+		    }
+		  
+		    imgResize();
+		    window.onresize = function () {
+			    imgResize()
+			};
+
+			function forceLeave() {
+				window.location.replace("<?php echo site_url(); ?>");
+			}
+
+			/*
+
+			Wait 30 seconds
+
+			Decide the number of jump scares and if the user gets to ask a question
+
+			10% chance of user asking
+
+			Begin question dialogue
+
+			User picks yes or no to asking
+
+			User picks question
+
+			Answer provided, jump scare sends user away
+
+			*/
+
+		</script>
 
 	<?php } ?>
-
-	<!-- You belong to your father, the devil, and you want to carry out your father’s desires. -John 8:44 -->
-
-	<script>
-
-		document.getElementById('ground').ondragstart = function() { return false; };
-		document.getElementById('left-corner').ondragstart = function() { return false; };
-		document.getElementById('right-corner').ondragstart = function() { return false; };
-
-	    var audio = document.getElementById("darkness");
-	    audio.volume = 0.2
-	    ;
-
-	    function imgResize() {
-	  	    var myHeight = 0;
-		    if( typeof( window.innerWidth ) == 'number' ) {
-		      //Non-IE
-		      myHeight = window.innerHeight;
-		    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-		      //IE 6+ in 'standards compliant mode'
-		      myHeight = document.documentElement.clientHeight;
-		    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-		      //IE 4 compatible
-		      myHeight = document.body.clientHeight;
-		    }
-		    var img = document.getElementById('ground');
-		    var imgheight = myHeight + 'px';
-		    img.style.height = imgheight;
-	    }
-	  
-	    imgResize();
-	    window.onresize = function () {
-		    imgResize()
-		};
-
-		function forceLeave() {
-			window.location.replace("<?php echo site_url(); ?>");
-		}
-
-		/*
-
-		Wait 30 seconds
-
-		Decide the number of jump scares and if the user gets to ask a question
-
-		10% chance of user asking
-
-		Begin question dialogue
-
-		User picks yes or no to asking
-
-		User picks question
-
-		Answer provided, jump scare sends user away
-
-		*/
-
-	</script>
 
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/dairyboycomics/shrek-souls.php
+++ wp-content/themes/dairyboycomics/shrek-souls.php
@@ -16,54 +16,74 @@ Template Name: Shrek Souls
 </head>
 <body <?php body_class(); ?>>
 
-	<a href="<?php echo site_url(); ?>">Leave this place knowing more than you did before</a>
+	<?php
+	if ( wp_is_mobile() ) { ?>
+	    <h1>Your device cannot fathom the information contained within this page. Redirecting you back to home...</h1>
 
-	<img id="shrekSouls" onclick="importantMessage()"" 
-	src="<?php echo get_stylesheet_directory_uri() ?>/img/shrek-souls.jpg" 
-	alt="There is undeniable proof that Shrek and Dark Souls takes place in the same universe"
-	title="There is undeniable proof that Shrek and Dark Souls takes place in the same universe">
+	    <a href="<?php echo site_url(); ?>">
+	    	<h1>If redirection doesn't work, click here</h1>
+	    </a>
 
-	<audio id="shrekSong" 
-	src="<?php echo get_stylesheet_directory_uri() ?>/media/Im_On_My_Way.mp3" 
-	autoplay loop></audio>
+	    <script>
+	    	function forceLeave() {
+				window.location.replace("<?php echo site_url(); ?>");
+			}
+			setTimeout(function () {
+		        //forceLeave();
+		    }, 3000);
+	    </script>
+	<?php } else { ?>
 
-	<!-- If you're seeing this message, you deserve to know the truth. Garbage memes have been slowly deteriorating my state of mind since high school and there's no going back. I'm scared. -->
+		<a href="<?php echo site_url(); ?>" id="turnBack">Leave this place knowing more than you did before</a>
 
-	<script>
-	    var audio = document.getElementById("shrekSong");
-	    audio.volume = 0.1;
+		<img id="shrekSouls" onclick="importantMessage()"" 
+		src="<?php echo get_stylesheet_directory_uri() ?>/img/shrek-souls.jpg" 
+		alt="There is undeniable proof that Shrek and Dark Souls takes place in the same universe"
+		title="There is undeniable proof that Shrek and Dark Souls takes place in the same universe">
 
-	    function importantMessage() {
-	    	alert("You better check yourself before you Shrek yourself");
-	    }
+		<audio id="shrekSong" 
+		src="<?php echo get_stylesheet_directory_uri() ?>/media/Im_On_My_Way.mp3" 
+		autoplay loop></audio>
 
-	    function imgResize() {
-	  	    var myHeight = 0;
-		    if( typeof( window.innerWidth ) == 'number' ) {
-		      //Non-IE
-		      myHeight = window.innerHeight;
-		    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-		      //IE 6+ in 'standards compliant mode'
-		      myHeight = document.documentElement.clientHeight;
-		    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-		      //IE 4 compatible
-		      myHeight = document.body.clientHeight;
+		<!-- If you're seeing this message, you deserve to know the truth. Garbage memes have been slowly deteriorating my state of mind since high school and there's no going back. I'm scared. -->
+
+		<script>
+		    var audio = document.getElementById("shrekSong");
+		    audio.volume = 0.1;
+
+		    function importantMessage() {
+		    	alert("You better check yourself before you Shrek yourself");
 		    }
-		    var img = document.getElementById('shrekSouls');
-		    var imgheight = myHeight + 'px';
-		    img.style.height = imgheight;
-	    }
-	  
-	    imgResize()
-	    window.onresize = function () {
+
+		    function imgResize() {
+		  	    var myHeight = 0;
+			    if( typeof( window.innerWidth ) == 'number' ) {
+			      //Non-IE
+			      myHeight = window.innerHeight;
+			    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+			      //IE 6+ in 'standards compliant mode'
+			      myHeight = document.documentElement.clientHeight;
+			    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+			      //IE 4 compatible
+			      myHeight = document.body.clientHeight;
+			    }
+			    var img = document.getElementById('shrekSouls');
+			    var imgheight = myHeight + 'px';
+			    img.style.height = imgheight;
+		    }
+		  
 		    imgResize()
-		};
+		    window.onresize = function () {
+			    imgResize()
+			};
 
-		function forceLeave() {
-			window.location.replace("<?php echo site_url(); ?>");
-		}
+			function forceLeave() {
+				window.location.replace("<?php echo site_url(); ?>");
+			}
 
-	</script>
+		</script>
+
+	<?php } ?>
 
 <?php wp_footer(); ?>
 </body>
